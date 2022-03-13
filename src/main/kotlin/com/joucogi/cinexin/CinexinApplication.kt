671e40af45f3fadf5024db747532d877b8437fac
Joel Coll <joel.coll@gmail.com>
2022-03-13 12:04:28 +0100

    Init a Spring project

---
+++ src/main/kotlin/com/joucogi/cinexin/CinexinApplication.kt
@@ -0,0 +1,11 @@
+package com.joucogi.cinexin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CinexinApplication
+
+fun main(args: Array<String>) {
+    runApplication<CinexinApplication>(*args)
+}
---
+++ src/test/kotlin/com/joucogi/cinexin/CinexinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joucogi.cinexin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CinexinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
